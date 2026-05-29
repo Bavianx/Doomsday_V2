961970b564ddd2d815770d04bf30edd31c2c28d0
Mochi x <Taviandc08@gmail.com>
2026-05-29 18:22:07 +0100

    V2: Live NewsAPI search connected - React fetches Django which queries NewsAPI (pre-design)

--- backend/threats/urls.py
+++ backend/threats/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
+    path('search/', views.search, name='search'),
 ]
--- backend/threats/views.py
+++ backend/threats/views.py
@@ -1,3 +1,5 @@
+import requests
+from decouple import config
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,3 +8,29 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def health_check(request):
     return Response({'status': 'Doomsday V2 API is running'})
+
+@api_view(['GET'])
+def search(request):
+    query = request.GET.get('q', '')
+
+    if not query:
+        return Response({'results': []})
+    
+    news_api_key = config('NEWS_API_KEY')
+    response = requests.get(
+        f'https://newsapi.org/v2/everything?q="{query}"&language=en&pageSize=10&sortBy=publishedAt&apiKey={news_api_key}'
+    )
+    articles = response.json().get('articles', [])
+    
+    results = [
+        {
+            'title': article.get('title', ''),
+            'source': article.get('source', {}).get('name', ''),
+            'url': article.get('url', '')
+        }
+        for article in articles
+    ]
+    
+    return Response({'query': query, 'results': results})
+
+        
